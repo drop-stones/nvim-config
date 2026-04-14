385ee764c19cf76cb9ee67aa830a867b60cd020f
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2026-04-14 19:13:07 +0900

    feat(im-switch): update config

--- lua/plugins/coding/im-switch.lua
+++ lua/plugins/coding/im-switch.lua
@@ -1,25 +1,15 @@
 return {
-  -- Auto IME switch
+  -- Auto IM switch
   {
     "drop-stones/im-switch.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
     event = "VeryLazy",
     opts = {
-      default_im_events = { "VimEnter", "InsertEnter", "FocusGained", "InsertLeave", "CmdlineLeave" },
-      save_im_state_events = {},
-      restore_im_events = {},
-      windows = {
-        enabled = true,
-      },
+      mode = "fixed",
       macos = {
-        enabled = true,
         default_im = "com.apple.keylayout.ABC",
       },
       linux = {
-        enabled = true,
         default_im = "keyboard-us",
-        get_im_command = { "fcitx5-remote", "-n" },
-        set_im_command = { "fcitx5-remote", "-s" },
       },
     },
   },
